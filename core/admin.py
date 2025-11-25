4f75bc034ac8a2510bf946eb3b262f49a3e7c15f
rodrigobrito23 <rodrigo_brito11@hotmail.com>
2025-11-25 09:42:10 -0300

    Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'atualizado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'atualizado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'atualizado')
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
    path('', IndexView.as_view(), name='index'),   
